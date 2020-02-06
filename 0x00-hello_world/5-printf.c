526f544597e041c848d1797f12dd06bf8390fbd9
Karijona <info@ernestoaponte.com>
2020-02-06 04:17:12 +0000

    printf string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - printf command with string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
